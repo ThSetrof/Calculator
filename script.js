aae86de910bd3df48ab5ff4cc4abf396192d6418
Tommy The Turtle <thsetrof@tuta.io>
2023-10-04 11:06:11 +0100

    Added simple and clean style to calculator hmtl

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(a, b){ return a + b};
+
+function subtract(a, b){ return a - b};
+
+function multiply(a, b){ return a * b};
+
+function divide(a, b){ return a / b};
+
+
+function operate(operator, a, b){
+    switch(operator){
+        case '+': return add(a, b);
+        case '-': return subtract(a, b);
+        case '*': return multiply(a, b);
+        case '÷': return divide(a, b);
+        default: return NaN;
+    }
+}
+
+let leftOperand, rightOperand, operator;
+
